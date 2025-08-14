f541dcaf807031dbc41e60b686dd44fc0a6104b4
Hafizh-al <h4f1zh2838@gmail.com>
2025-08-14 21:16:43 +0700

    first commit

---
+++ pertemuan 15.py
@@ -0,0 +1,57 @@
+# Latihan 1: Membuat dan Mengakses
+# -. Buat sebuah dictionary bernama biodata untuk merepresentasikan dirimu. Isinya harus memiliki
+# key: "nama", "umur", "hobi" (hobi bisa berupa list dari beberapa string), dan
+# "sudah_menikah" (berisi boolean).
+# /. Cetak seluruh dictionary.
+# 0. Cetak hanya value dari key "nama".
+# 1. Cetak hobi pertamamu dari list hobi.
+biodata = {
+    "nama": "Muhammad Daffa Al Hafizh",
+    "umur": 17,
+    "hobi": ["Membaca", "Coding", "Olahraga", "Traveling", "Watching", "etc"],
+    "sudah_menikah": False
+}
+print(biodata)                # Cetak semua
+print(biodata["nama"])        # Ambil nama
+print(biodata["hobi"][0])     # Ambil hobi pertama
+# Latihan 2: Modiikasi Dictionary
+# -. Gunakan dictionary biodata dari Latihan 1.
+# /. Tambahkan pasangan key-value baru: "kota": "Nama Kotamu".
+# 0. Ubah value dari key "umur" menjadi umurmu tahun depan.
+# 1. Cetak dictionary yang sudah diperbarui.
+biodata = {
+    "nama": "Muhammad Daffa Al Hafizh",
+    "umur_tahun_depan": 18,
+    "hobi": ["Membaca", "Coding", "Olahraga", "Traveling", "Watching", "etc"],
+    "sudah_menikah": False,
+    "kota": "Purwokerto"
+}
+print(biodata) # dictionary yang sudah diperbarui.
+# Latihan 3: Penggunaan .get()
+# -. Masih dengan dictionary biodata.
+# /. Coba akses key "pekerjaan" menggunakan bracket notation []. Apa yang terjadi? (Beri komentar
+# pada baris ini agar tidak error).
+# 0. Sekarang, akses key "pekerjaan" menggunakan metode .get(). Cetak hasilnya.
+# 1. Akses lagi key "pekerjaan" menggunakan .get(), tapi kali ini berikan nilai default "Pelajar".
+# Cetak hasilnya.
+# print(biodata["pekerjaan"])  # Akan error → KeyError
+
+print(biodata.get("pekerjaan"))               # None
+print(biodata.get("pekerjaan", "Pelajar"))    # Pelajar
+
+# Latihan 4: Histogram Kata
+# Buat program yang:
+# -. Meminta pengguna memasukkan sebuah kalimat.
+# /. Membuat histogram (dalam bentuk dictionary) yang menghitung frekuensi setiap kata (bukan
+# huruf) dalam kalimat tersebut.
+# 0. Cetak dictionary histogram tersebut.
+# Petunjuk: Gunakan metode .split() untuk mengubah kalimat menjadi list kata-kata terlebih
+# dahulu. Abaikan huruf besar/kecil dengan mengubah seluruh kalimat menjadi lowercase di awal
+kalimat = input("Masukkan kalimat: ").lower()
+kata_list = kalimat.split()
+
+histogram = {}
+for kata in kata_list:
+    histogram[kata] = histogram.get(kata, 0) + 1
+
+print(histogram)
